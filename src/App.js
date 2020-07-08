31afcd28fd9340dc4ea5ac4ee0459a116a17e61c
Gawtham R <gawtham.r@ad.infosys.com>
2020-07-08 22:11:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Youtube from "./api";
+import { SearchBar, VideoDetail, VideoList } from "./components";
+export class App extends Component {
+  state = {
+    videos: [],
+    selectedVideo: "",
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  handleSubmit = async (searchTerm) => {
+    const response = await Youtube.get("search", {
+      params: {
+        part: `snippet`,
+        maxResults: 10,
+        key: `AIzaSyD-cXV2oaIzouIKdeHnssuI_5QtgXJTsxU`,
+        q: searchTerm,
+      },
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+  render() {
+    const { selectedVideo, videos } = this.state;
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail video={selectedVideo} />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
